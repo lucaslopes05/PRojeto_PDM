3bb8afebff1f40e0070cb0c22d07e98d2fde5149
Lucas Andre <lucaslopes05@gmail.com>
2020-04-18 07:50:22 -0300

    iniciando projeto

---
+++ app/src/main/java/com/example/receitas/view/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.receitas.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.receitas.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
